58dbd3f32014c5b0f0c2528f21ed87a3e6ca9d0d
Chris Bistline <chris.bistline@gmail.com>
2019-05-21 18:06:15 -0500

    fixed error in scraper skipping files

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,6 @@
-// REQUIRED FILES
+// Router setup
 var express = require("express");
 var router = express.Router();
-// var mongoose = require("mongoose");
-
 
 // Scraping tools
 var axios = require("axios");
@@ -11,96 +9,72 @@ var cheerio = require("cheerio");
 // Require all models
 var db = require("../models");
 
-
 // Routes
+
+// "/" route for homepage
 router.get("/", (req, res) => {
     res.render("index");
 });
 
+// scrape route to collect data from MLB.com
 router.get("/scrape", function (req, res) {
-    scrapedData = scrape();
-    res.json("scrape complete");
+    var scrapedData = scrape();
+    console.log(scrapedData);
+    res.json(scrapedData);
 });
 
 // scraper function
 function scrape() {
-    axios.get("https://www.mlb.com").then(function (response) {
+    axios.get("https://www.mlb.com")
+        .then(function (response) {
 
-        // Load the HTML into cheerio and save it to a variable
-        // '$' becomes a shorthand for cheerio's selector commands, much like jQuery's '$'
-        var $ = cheerio.load(response.data);
+            // Load the HTML into cheerio and save it to a variable
+            var $ = cheerio.load(response.data);
 
-        // An empty array to save the data that we'll scrape
-        // var results = [];
+            // scrape HTML fro headline links
+            $("a.p-headline-stack__link").each(function (i, element) {
 
-        // Select each element in the HTML body from which you want information.
-        // NOTE: Cheerio selectors function similarly to jQuery's selectors,
-        // but be sure to visit the package's npm page to see how it works
-        $("a.p-headline-stack__link").each(function (i, element) {
+                var title = $(element).text();
+                var link = $(element).attr("href");
 
-            var title = $(element).text();
-            var link = $(element).attr("href");
-
-            // Save these results in an object that we'll push into the results array we defined earlier
-            db.Article.create({
-                title: title,
-                link: link
-            })
-                .then(function (dbArticle) {
-                    // View the added result in the console
-                    console.log(dbArticle);
+                // Save these results in an object that we'll push into the results array we defined earlier
+                db.Article.create({
+                    title: title,
+                    link: link
                 })
-                .catch(function (err) {
-                    // If an error occurred, log it
-                    console.log(err);
-                });
-
+                    .then(function (dbArticle) {
+                        // View the added result in the console
+                        console.log("Article inserted");
+                        return dbArticle;
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, log it
+                        console.log(err);
+                    });
+            });
         });
-    });
 }
 
-
+// delete articles in collection
+router.get("/clear", function (req, res) {
+    db.Article.deleteMany({})
+        .then(function (dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+            console.log("documents deleted");
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, log it
+            console.log(err);
+        });
+});
 
 
 
 
 /*
-// A GET route for scraping the echoJS website
-app.get("/scrape", function(req, res) {
-  // First, we grab the body of the html with axios
-  axios.get("http://www.echojs.com/").then(function(response) {
-    // Then, we load that into cheerio and save it to $ for a shorthand selector
-    var $ = cheerio.load(response.data);
 
-    // Now, we grab every h2 within an article tag, and do the following:
-    $("article h2").each(function(i, element) {
-      // Save an empty result object
-      var result = {};
-
-      // Add the text and href of every link, and save them as properties of the result object
-      result.title = $(this)
-        .children("a")
-        .text();
-      result.link = $(this)
-        .children("a")
-        .attr("href");
-
-      // Create a new Article using the `result` object built from scraping
-      db.Article.create(result)
-        .then(function(dbArticle) {
-          // View the added result in the console
-          console.log(dbArticle);
-        })
-        .catch(function(err) {
-          // If an error occurred, log it
-          console.log(err);
-        });
-    });
-
-    // Send a message to the client
-    res.send("Scrape Complete");
-  });
-});
 
 // Route for getting all Articles from the db
 app.get("/articles", function(req, res) {
